6a5838908feab8a00237905a84a0b9fcc07baefe
JulienPetit-1 <56109194+JulienPetit-1@users.noreply.github.com>
2020-06-26 00:06:30 +0200

    Update scrapping and add unit tests

---
+++ app_tests/units/test_scrapping.py
@@ -0,0 +1,16 @@
+import unittest
+from app import Scrapping
+
+
+class TestScrapping(unittest.TestCase):
+    def test_extract_players_info():
+        # Assign
+        test_link = f"https://int.soccerway.com/players/paul-pogba/177208/"
+        html = Scrapping.get_html_from_page(test_link) 
+        expected_output = ('P. Pogba', 'Manchester United', 105.0, 'Midfielder', 0, 627, 9, 0, 'doubtful', 0.0, 'https://secure.cache.images.core.optasports.com/soccer/players/18x18/420221.png')
+
+        # Acts
+        output = Scrapping.extract_players_info(html)
+
+        # Assert
+        assert expected_output == output
